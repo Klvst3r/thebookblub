06d955e91ae32c006fbf1da2bc8f09b23573070b
Klvst3r <klvst3r@gmail.com>
2025-06-24 10:30:32 -0600

    5.6 Relaciones de tablas en BD

--- models/Author.php
+++ models/Author.php
@@ -43,5 +43,35 @@ class Author extends ActiveRecord {
                [['name'], 'string', 'max' => 255],
          ];
       }
+
+      public function getVotes($book_id = null){
+         $query = $this->hasMany(BookScore::class, ['book_id' => 'book_id'])
+         ->viaTable('books', ['author_id' => 'author_id']);
+
+         if(!empty($book_id)){
+            $query = $query->where(['book_id' => '$book_id']);
+         }
+         return $query->all();
+      }
    
+      public function getScore($book_id = null){
+         //return 1.2;
+         //return count($this->votes); //solo para comprobar el prmedio
+
+         $i = 0;
+         $sum = 0;
+         $arr = $this->votes;
+         if(!empty($book_id)){
+            $arr = $this->getVotes($book_id);
+         }
+         //foreach($this->getVotes(218) as $vote) { //para el ejercicio
+         foreach($this->votes as $vote) {
+            $i++;
+            $sum += $vote->score;
+         }
+         if($i == 0){
+            return 'Sin votos';
+         }
+         return sprintf("%0.2f (%d votos)", $sum/$i, $i);
+      }
 }
